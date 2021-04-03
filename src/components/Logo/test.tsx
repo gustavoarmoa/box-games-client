e12e71585e59b621efa48acf7a6010aa5748ec81
gustavoarmoa <gustavo.armoa@outlook.com>
2021-04-03 16:37:06 -0300

    Primeiro teste de cor da logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    // renderizar o componente (render do reactlibrary)
+    // selecionar o elemento que queremos testar com o metdo (screem)
+    // exception - assertion - asserção - comparação
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFa'
+    })
   })
 })
