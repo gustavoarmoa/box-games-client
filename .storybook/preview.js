189175db405863ed361a55d9c4638bb6a7774f79
gustavoarmoa <gustavo.armoa@outlook.com>
2021-04-03 13:30:51 -0300

    Add theme path to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,13 @@ import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
 import GlobalStyles from '../src/styles/global'
 import { ThemeProvider } from 'styled-components'
+import theme from '../src/styles/theme'
 
 addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
     </ThemeProvider>
