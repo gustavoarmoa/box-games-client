172330de51afb394a0986d164599fa480ab944b5
gustavoarmoa <gustavo.armoa@outlook.com>
2021-05-04 07:27:11 -0300

    Update snapshots

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -25,9 +25,9 @@ const wrapperModifiers = {
   `,
 
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
+    /* display: inline-flex;
+      align-items: center;
+      justify-content: center; */
 
     svg {
       width: 1.5rem;
@@ -41,12 +41,16 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     cursor: pointer;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
 
     &:hover {
       background: linear-gradient(180deg, #e35564 0%, #d958a6 50%);
