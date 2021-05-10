55171c594fad9908d509c183031d2ae231c69597
gustavoarmoa <gustavo.armoa@outlook.com>
2021-05-10 07:35:37 -0300

    Add image and ajust const

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead it is back',
+    subtitle: 'Come see Johns new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/random'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Highlight from '.'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/random'
+}
+
+describe('<Highlight />', () => {
+  it('should render heading and button', () => {
+    // const { container } =
+    renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+})
